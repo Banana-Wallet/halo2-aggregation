eaf66bcf3bcdefb74c02f11df34901b89062aa9a
Rishabh Gupta <rishotics@gmail.com>
2024-01-25 16:10:06 +0530

    add proper circuit test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 
 // use super::*;
 use halo2_ecc::{
-    bn254::{pairing::{PairingChip}, Fp12Chip, FpChip}, fields::FieldChip, halo2_base};
+    bn254::{self, pairing::{PairingChip}, Fp12Chip, FpChip}, fields::FieldChip, halo2_base};
 use halo2_ecc::{
     // fields::FpStrategy
     fields::FpStrategy, 
@@ -27,17 +27,15 @@ use halo2_base::{
     Context, halo2_proofs::{
         dev::{
             metadata::Column, MockProver
-        }, 
-        halo2curves::{
+        }, halo2curves::{
             bn256::{
                 pairing, G1Affine, G2Affine
             }
-        }, 
-        plonk::{
+        }, plonk::{
             Selector, Advice
-        }
+        }, poly::commitment::Prover
     },
-    halo2_proofs::halo2curves::bn256::Fr
+    halo2_proofs::halo2curves::bn256::{self, Fr}
 };
 
 // use halo2curves::bn256::Fr;
@@ -107,36 +105,77 @@ impl<F: BigPrimeField> TestCircuit<F> {
     }
 }
 
-
-#[cfg(test)]
-mod tests {
-    use std::path;
-    use halo2_base::gates::circuit::CircuitBuilderStage;
-
-    use super::*;
-    // use crate::bn254::tests::pairing::PairingCircuitParams;
-
-    
-    // #[test]
-    // fn test_pp() {
-    //     const K: u32 = 9;
-    //     // let params: ParamsKZG
-    //     let path = "configs/bn254/pairing_circuit.config";
-    //     let params: PairingCircuitParams = serde_json::from_reader(
-    //         File::open(path).unwrap_or_else(|e| panic!("{path} does not exist: {e:?}")),
-    //     )
-    //     .unwrap();
-    //     let lookup_bits = params.lookup_bits;
-    //     let mut stage = CircuitBuilderStage::Mock;
-    //     let mut builder = BaseCircuitBuilder::<Fr>::from_stage(stage)
-    //         .use_k(K as usize)
-    //         .set_lookup_bits(lookup_bits);
-    //     // let range
-    //     }
+pub trait AppCircuit<F: BigPrimeField> {
+    fn create_circuit(
+        stage: CircuitBuilderStage,
+        params: PairingCircuitParams,
+        P: G1Affine,
+        Q: G2Affine,
+    ) -> Result<BaseCircuitBuilder<bn256::Fr>, Error>;
 }
+
+impl <F: BigPrimeField> AppCircuit<F> for TestCircuit<F> {
+    fn create_circuit(
+        stage: CircuitBuilderStage,
+        params: PairingCircuitParams,
+        P: G1Affine,
+        Q: G2Affine,
+    ) -> Result<BaseCircuitBuilder<bn256::Fr>, Error> {
+
+        let k = params.degree as usize;
+
+        let mut builder = BaseCircuitBuilder::<Fr>::from_stage(stage).use_k(params.degree as usize);
+        // builder.use_k(params.degree as usize);
+        // builder.set_lookup_bits(params.lookup_bits);
+        // MockProver::run(9, &builder, vec![]).unwrap().assert_satisfied();
+        // if let Some(lb) = params.lookup_bits {
+            // builder.set_lookup_bits(params.lookup_bits);
+        // }
+        let range = RangeChip::new(params.lookup_bits, builder.lookup_manager().clone());
+
+        let ctx = builder.main(0);
+        // // run the function, mutating `builder`
+
+        let res = pairing_test(ctx, &range, params, P, Q);
+
+        // // helper check: if your function didn't use lookups, turn lookup table "off"
+        let t_cells_lookup =
+            builder.lookup_manager().iter().map(|lm| lm.total_rows()).sum::<usize>();
+        let lookup_bits = if t_cells_lookup == 0 { None } else { std::option::Option::Some(params.lookup_bits) };
+        builder.set_lookup_bits(params.lookup_bits);
+
+        // // // configure the circuit shape, 9 blinding rows seems enough
+        builder.calculate_params(Some(9));
+
+        Ok((builder))
+        
+    }
+}
+
+
 
 #[test]
 
+
+fn test_pairing_circuit() {
+
+    let path = "/Users/rishabh/projects/blockchain/avail-project/halo2-aggregation/src/configs/bn254/pairing_circuit.config";
+    let params: PairingCircuitParams = serde_json::from_reader(
+        File::open(path).unwrap_or_else(|e| panic!("{path} does not exist: {e:?}")),
+    )
+    .unwrap();
+    let mut rng = StdRng::seed_from_u64(0);
+    let P = G1Affine::random(&mut rng);
+    let Q = G2Affine::random(&mut rng);
+    let circuit = TestCircuit::<Fr>::create_circuit(
+        CircuitBuilderStage::Mock,
+        params, 
+        P, 
+        Q).unwrap();
+    MockProver::run(params.degree, &circuit, vec![]).unwrap().assert_satisfied();
+    
+    // let prover = MockProver::<Fr>::run(9, &circuit, vec![]).unwrap();
+}
 
 fn test_pairing() {
     
